a788b71fc993edb90efe2be7c9830bdce6ffde41
Víctor <vgomtej353@g.educaand.es>
2026-03-03 14:19:26 +0100

    fix: set default values for database connection parameters

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -2,7 +2,6 @@ import "reflect-metadata";
 import { DataSource } from "typeorm";
 import * as dotenv from "dotenv";
 
-// Importante: cargar el config() lo primero de todo
 dotenv.config(); 
 
 console.log("Intentando conectar con usuario:", process.env.DB_USERNAME); // Línea de prueba
@@ -11,10 +10,10 @@ export const AppDataSource = new DataSource({
     type: "postgres",
     host: process.env.DB_HOST || "localhost",
     port: Number(process.env.DB_PORT) || 5432,
-    username: process.env.DB_USERNAME,
-    password: String(process.env.DB_PASSWORD), // Forzamos a que sea string
-    database: process.env.DB_NAME,
+    username: process.env.DB_USERNAME || "admin",
+    password: String(process.env.DB_PASSWORD) || "temporal123",
+    database: process.env.DB_NAME || "trazapiezas_db",
     synchronize: true,
-    logging: true, // Cámbialo a true para ver cómo se crean las tablas en la consola
+    logging: true,
     entities: [__dirname + "/entities/*.ts"],
 });
