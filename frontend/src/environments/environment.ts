48eb1dd45033d507616975552132121c52bfaa9e
Víctor <vgomtej353@g.educaand.es>
2026-05-18 10:39:19 +0200

    Update baseUrl in environment configuration for Vercel deployment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    baseUrl: 'https://trazapiezas.com', // Cambia esto a tu URL de producción de vercel
+    baseUrl: 'depliegue-front-trazapiezas.vercel.app', // Cambia esto a tu URL de producción de vercel
     apiUrl: 'https://trazapiezas.com/api', // Cambia esto a tu URL de producción de render
 };
