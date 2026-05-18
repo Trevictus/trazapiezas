f4ffc4d0c02d208be14769fc6dd9d251383ccb62
Víctor <vgomtej353@g.educaand.es>
2026-05-18 08:58:23 +0200

    Add Vercel configuration for rewrites

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    baseUrl: 'https://trazapiezas.com',
-    apiUrl: 'https://trazapiezas.com/api',
+    baseUrl: 'https://trazapiezas.com', // Cambia esto a tu URL de producción de vercel
+    apiUrl: 'https://trazapiezas.com/api', // Cambia esto a tu URL de producción de render
 };
