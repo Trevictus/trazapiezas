0a40816756de118f88101baea4c8fcd81afd107e
Víctor <vgomtej353@g.educaand.es>
2026-05-18 17:23:43 +0200

    add api route to production environment

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     baseUrl: 'https://trazapiezas.vercel.app/', // Cambia esto a tu URL de producción de vercel
-    apiUrl: 'https://trazapiezas-production.up.railway.app', // Cambia esto a tu URL de producción de render
+    apiUrl: 'https://trazapiezas-production.up.railway.app/api', // Cambia esto a tu URL de producción de render
 };
